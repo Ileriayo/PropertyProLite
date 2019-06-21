ca23c307fa5d844bb501a922b5590c5fe11d3cae
unknown <ileriayoadebiyi@gmail.com>
2019-06-21 02:14:07 +0100

    feature(view-properties): design all properties page [Finishes #166626531]

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,27 +1,32 @@
 /* HAMBURGER & DROPDOWN */
-const hamburger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.navbar__menu-mobile');
-const mobileMenu2 = document.querySelector('.navbar__menu-mobile--margin');
+(() => {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.navbar__menu-mobile');
+    const mobileMenu2 = document.querySelector('.navbar__menu-mobile--margin');
 
-const changeDisplayState = (element) => {
-    if (!element) return;
-    if (element.style.display === "block") {
-        element.style.display = "none";
-    } else {
-        element.style.display = "block";
-    }
-};
+    const changeDisplayState = (element) => {
+        if (!element) return;
+        if (element.style.display === "block") {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+        }
+    };
 
-hamburger.addEventListener('click', () => {
-    changeDisplayState(mobileMenu);
-    changeDisplayState(mobileMenu2);
-});
+    hamburger.addEventListener('click', () => {
+        changeDisplayState(mobileMenu);
+        changeDisplayState(mobileMenu2);
+    });
 
-const dropdownArrow = document.querySelector('.dropdown-arrow');
-const profileMenu = document.querySelector('.navbar__menu-profile');
-
-dropdownArrow.addEventListener('click', () => {
-    changeDisplayState(profileMenu);
-});
+    (() => {
+        const dropdownArrow = document.querySelector('.dropdown-arrow');
+        const profileMenu = document.querySelector('.navbar__menu-profile');
+        
+        if (!dropdownArrow) return;
+        dropdownArrow.addEventListener('click', () => {
+            changeDisplayState(profileMenu);
+        });
+    })();
+})();
 
 /* END HAMBURGER & DROPDOWN */

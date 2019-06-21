d84c1c5a43684188ef960e2009d41e10d5561310
unknown <ileriayoadebiyi@gmail.com>
2019-06-21 01:49:43 +0100

    feature(view-properties): design all properties page [Finishes #166626531]

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,11 +1,27 @@
-/* HAMBURGER */
+/* HAMBURGER & DROPDOWN */
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.navbar__menu-mobile');
-hamburger.addEventListener('click', () => {
-    if (mobileMenu.style.display === "block") {
-        mobileMenu.style.display = "none";
+const mobileMenu2 = document.querySelector('.navbar__menu-mobile--margin');
+
+const changeDisplayState = (element) => {
+    if (!element) return;
+    if (element.style.display === "block") {
+        element.style.display = "none";
     } else {
-        mobileMenu.style.display = "block";
+        element.style.display = "block";
     }
-})
-/* END HAMBURGER */
+};
+
+hamburger.addEventListener('click', () => {
+    changeDisplayState(mobileMenu);
+    changeDisplayState(mobileMenu2);
+});
+
+const dropdownArrow = document.querySelector('.dropdown-arrow');
+const profileMenu = document.querySelector('.navbar__menu-profile');
+
+dropdownArrow.addEventListener('click', () => {
+    changeDisplayState(profileMenu);
+});
+
+/* END HAMBURGER & DROPDOWN */

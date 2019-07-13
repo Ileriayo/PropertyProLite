865c6133e53376779469429bbc7f51b08c20f0be
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2019-07-13 12:25:13 +0100

    feature(update-price): setup endpoint to update price [Finishes #167070220]

--- API/middlewares/validate.js
+++ API/middlewares/validate.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import ValidationHelpers from '../helpers/validationHelpers';
 import regex from '../utils/regexes';
 
@@ -10,15 +11,15 @@ class Validate {
   static async checkRequestBody(req, res, next) {
     const errors = [];
     const {
-      firstName, lastName, email, password, phoneNumber, address,
+      first_name, last_name, email, password, phone_number, address,
     } = req.body;
     if (req.path.includes('signup')) {
-      errors.push(...checkForEmptyField('First name', firstName));
-      errors.push(...checkFieldLength('First name', firstName, 3));
-      errors.push(...checkForEmptyField('Last name', lastName));
-      errors.push(...checkFieldLength('Last name', lastName, 3));
-      errors.push(...checkForEmptyField('Phone Number', phoneNumber));
-      errors.push(...checkFieldPattern('Phone Number', phoneNumber, phoneNumberRegex));
+      errors.push(...checkForEmptyField('First name', first_name));
+      errors.push(...checkFieldLength('First name', first_name, 3));
+      errors.push(...checkForEmptyField('Last name', last_name));
+      errors.push(...checkFieldLength('Last name', last_name, 3));
+      errors.push(...checkForEmptyField('Phone Number', phone_number));
+      errors.push(...checkFieldPattern('Phone Number', phone_number, phoneNumberRegex));
       errors.push(...checkForEmptyField('Address', address));
       errors.push(...checkFieldPattern('Password', password, passwordRegex));
     }
@@ -35,10 +36,25 @@ class Validate {
     return next();
   }
 
+  static async checkUpdatePrice(req, res, next) {
+    const errors = [];
+    const { price } = req.body;
+    errors.push(...checkForEmptyField('Price', price));
+    errors.push(...checkFieldPattern('Price', price, numberRegex));
+
+    if (errors.length) {
+      return res.status(400).json({
+        status: 'Error: Field(s) require your attention',
+        error: errors,
+      });
+    }
+    return next();
+  }
+
   static async checkProperty(req, res, next) {
     const errors = [];
     const {
-      price, state, city, address, type, imageUrl,
+      price, state, city, address, type, image_url,
     } = req.body;
     errors.push(...checkForEmptyField('Price', price));
     errors.push(...checkFieldPattern('Price', price, numberRegex));
@@ -46,7 +62,7 @@ class Validate {
     errors.push(...checkForEmptyField('City', city));
     errors.push(...checkForEmptyField('Address', address));
     errors.push(...checkForEmptyField('Type', type));
-    errors.push(...checkForEmptyField('Image', imageUrl));
+    errors.push(...checkForEmptyField('Image', image_url));
 
     if (errors.length) {
       return res.status(400).json({

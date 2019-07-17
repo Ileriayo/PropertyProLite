2ffe918398b7b2aaa6a1af41e6699e11ed71e124
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2019-07-17 14:31:53 +0100

    restart travis build

--- API/controllers/userDB.controller.js
+++ API/controllers/userDB.controller.js
@@ -1,9 +1,12 @@
 /* eslint-disable camelcase */
-import userDBModel from '../models/usersDB.models';
+// import userDBModel from '../models/usersDB.models';
 import HashPassword from '../helpers/hashPassword';
 import Tokenizer from '../helpers/tokenizer';
+import Query from '../db/index';
 
-const { addUser } = userDBModel;
+const { query } = Query;
+
+// const { addUser } = userDBModel;
 const { hashPassword } = HashPassword;
 const { tokenizer } = Tokenizer;
 
@@ -14,7 +17,13 @@ class UserController {
     } = req.body;
     try {
       const hashedPassword = await hashPassword(password);
-      const newUser = await addUser(`'${email}', '${first_name}', '${last_name}', '${hashedPassword}', '${phone_number}', '${address}', false`);
+      // const newUser = await addUser(`'${email}', '${first_name}', '${last_name}',
+      // '${hashedPassword}', '${phone_number}', '${address}', false`);
+      const querystring = `INSERT INTO users(email, first_name, last_name, password, phone_number, address, is_admin)
+       VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *;`;
+
+      const newUser = await query(querystring,
+        [email, first_name, last_name, hashedPassword, phone_number, address, false]);
       console.log('This user just signed up:', newUser);
       const { id } = newUser[0];
       const token = await tokenizer({ id });

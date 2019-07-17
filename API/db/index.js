f5c55e56ab70a3a2219601b43b6641397588117a
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2019-07-17 15:04:56 +0100

    restart travis build

--- API/db/index.js
+++ API/db/index.js
@@ -5,15 +5,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-/* CONNECTION TO LOCAL DB */
-// const pool = new Pool({
-//   user: process.env.PG_USER,
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DB,
-//   password: process.env.PG_PASSWORD,
-//   port: process.env.PG_PORT,
-// });
-
 /* CONNECTION TO PRODUCTION DB */
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
